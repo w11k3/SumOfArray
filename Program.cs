36e7bec0b549ce2fd9f7fa3a3707321bdd579b02
Paulo Kinjo <paulokinjo@hotmail.com>
2024-07-04 19:38:54 -0400

    Making Sum arrays

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿public class SumArray
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        int total = 0;
+
+        // add each element's value total
+        for (int counter = 0; counter < array.Length; ++counter)
+            total += array[counter];
+
+        Console.WriteLine($"Total of array elements: {total}");
+    } // end Main
+} // end class SumArray
